ee8ce8a5c03fb6796440d6a7c1d59b6c532852bd
Ramasamy Kasiviswanathan <kramasamy1991@gmail.com>
2019-10-19 23:17:17 -0400

    Code for RoboRace

---
+++ JPMCAM.c
@@ -0,0 +1,209 @@
+/*
+ * 3pi-linefollower - demo code for the Pololu 3pi Robot
+ * 
+ * This code will follow a black line on a white background, using a
+ * very simple algorithm.  It demonstrates auto-calibration and use of
+ * the 3pi IR sensors, motor control, bar graphs using custom
+ * characters, and music playback, making it a good starting point for
+ * developing your own more competitive line follower.
+ *
+ * http://www.pololu.com/docs/0J21
+ * http://www.pololu.com
+ * http://forum.pololu.com
+ *
+ */
+
+// The 3pi include file must be at the beginning of any program that
+// uses the Pololu AVR library and 3pi.
+#include <pololu/3pi.h>
+
+// This include file allows data to be stored in program space.  The
+// ATmega168 has 16k of program space compared to 1k of RAM, so large
+// pieces of static data should be stored in program space.
+#include <avr/pgmspace.h>
+
+#include <pololu/orangutan.h>
+
+// Introductory messages.  The "PROGMEM" identifier causes the data to
+// go into program space.
+const char welcome_line1[] PROGMEM = " Pololu";
+const char welcome_line2[] PROGMEM = "3\xf7 Robot";
+const char demo_name_line1[] PROGMEM = "JPMC-AM";
+const char demo_name_line2[] PROGMEM = "Team";
+
+// A couple of simple tunes, stored in program space.
+// const char welcome[] PROGMEM = ">g32>>c32";
+const char go[] PROGMEM = "L16 cdegreg4";
+
+// Data for generating the characters used in load_custom_characters
+// and display_readings.  By reading levels[] starting at various
+// offsets, we can generate all of the 7 extra characters needed for a
+// bargraph.  This is also stored in program space.
+const char levels[] PROGMEM = {
+	0b00000,
+	0b00000,
+	0b00000,
+	0b00000,
+	0b00000,
+	0b00000,
+	0b00000,
+	0b11111,
+	0b11111,
+	0b11111,
+	0b11111,
+	0b11111,
+	0b11111,
+	0b11111
+};
+
+// This function loads custom characters into the LCD.  Up to 8
+// characters can be loaded; we use them fointr 7 levels of a bar graph.
+void load_custom_characters()
+{
+	lcd_load_custom_character(levels+0,0); // no offset, e.g. one bar
+	lcd_load_custom_character(levels+1,1); // two bars
+	lcd_load_custom_character(levels+2,2); // etc...
+	lcd_load_custom_character(levels+3,3);
+	lcd_load_custom_character(levels+4,4);
+	lcd_load_custom_character(levels+5,5);
+	lcd_load_custom_character(levels+6,6);
+	clear(); // the LCD must be cleared for the characters to take effect
+}
+
+// This function displays the sensor readings using a bar graph.
+void display_readings(const unsigned int *calibrated_values)
+{
+	unsigned char i;
+
+	for(i=0;i<5;i++) {
+		// Initialize the array of characters that we will use for the
+		// graph.  Using the space, an extra copy of the one-bar
+		// character, and character 255 (a full black box), we get 10
+		// characters in the array.
+		const char display_characters[10] = {' ',0,0,1,2,3,4,5,6,255};
+
+		// The variable c will have values from 0 to 9, since
+		// calibrated values are in the range of 0 to 1000, and
+		// 1000/101 is 9 with integer math.
+		char c = display_characters[calibrated_values[i]/101];
+
+		// Display the bar graph character.
+		print_character(c);
+	}
+}
+
+void delayfunction()
+{
+	delay_ms(1000);
+}
+
+void initialize()
+{
+	unsigned int counter; // used as a simple timer
+	unsigned int sensors[5]; // an array to hold sensor values
+
+	// This must be called at the beginning of 3pi code, to set up the
+	// sensors.  We use a value of 2000 for the timeout, which
+	// corresponds to 2000*0.4 us = 0.8 ms on our 20 MHz processor.
+	pololu_3pi_init(2000);
+	load_custom_characters(); // load the custom characters
+	
+	// Play welcome music and display a message
+	print_from_program_space(welcome_line1);
+	lcd_goto_xy(0,1);
+	print_from_program_space(welcome_line2);
+	// play_from_program_space(welcome);
+	delayfunction();
+
+	clear();
+	print_from_program_space(demo_name_line1);
+	lcd_goto_xy(0,1);
+	print_from_program_space(demo_name_line2);
+	delayfunction();
+
+	while(!button_is_pressed(BUTTON_B))
+	{
+		unsigned int position = read_line(sensors,IR_EMITTERS_ON);
+		clear();
+		print_long(position);
+		lcd_goto_xy(0,1);
+		display_readings(sensors);
+		delayfunction();
+	}
+	wait_for_button_release(BUTTON_B);
+	delayfunction();
+
+	counter = 2;
+	while(counter-->0)
+	{
+		if(counter%2==0)
+			set_motors(40,-40);
+		else
+			set_motors(-40,40);
+		
+		calibrate_line_sensors(IR_EMITTERS_ON);
+		set_motors(-40,40);
+		delay_ms(20);
+	}
+	set_motors(0,0);
+	clear();
+	print("Go!");
+}
+
+void drivingAlgorithm(){
+	unsigned int sensors[5]; // an array to hold sensor values
+	unsigned int last_proportional=0;
+	long integral=0;
+	int track_course = 0;
+	while(1){
+	unsigned int position = read_line(sensors,IR_EMITTERS_ON);
+	//This is the Finish line code.
+	if(sensors[1]>600 && sensors[2]>600 && sensors[3]>600) //Setting The value to Zero, so that in case of any issue we can turn the robot. 
+		set_motors(0,0);
+		continue;
+	}
+
+	// if(sensors[0]>200 &&sen)
+	// The "proportional" term should be 0 when we are on the line.
+	int proportional = ((int)position) - 2000;
+
+	if(proportional == 0)
+		track_course++;
+	else if(proportional != 0 && track_course >4 ) // Step off from the pedal
+		track_course = 0;
+	// Compute the derivative (change) and integral (sum) of the
+	// position.
+	int derivative = proportional - last_proportional;
+	integral += proportional;
+
+	// Remember the last position.
+	last_proportional = proportional;
+
+	// Compute the difference between the two motor power settings,
+	// m1 - m2.  If this is a positive number the robot will turn
+	// to the right.  If it is a negative number, the robot will
+	// turn to the left, and the magnitude of the number determines
+	// the sharpness of the turn.
+	int power_difference = proportional/20 + integral/10000 + derivative*3/2;
+
+	// Compute the actual motor settings.  We never set either motor
+	// to a negative value.
+	int max = 100;
+	if(track_course >=2) //Go high speed only if you find it as a straight lane.
+		max = 160;
+	
+	if(power_difference > max)
+		power_difference = max;
+	if(power_difference < -max)
+		power_difference = -max;
+
+	if(power_difference < 0)
+		set_motors(max+power_difference, max);
+	else
+		set_motors(max, max-power_difference);
+}
+
+int main()
+{
+	initialize();
+}
